b062e408a643ebf42641be88caee07cfd0484fdf
damodara2006 <damodara2006@gmail.com>
2025-02-27 23:40:16 +0530

    Enhance Home page layout and styling with new background, profile image, and updated navigation

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,13 +1,13 @@
-import React from "react";
-
+import profile from "../assets/flake-2.BnWSExPC.png"
 function Home() {
   return (
-    <div>
-      <div className=" flex justify-between items-center h-14 bg-[linear-gradient(140deg,_#ff0000,_#0000ff)] rounded-[6px]">
-        <h1 className=" ml-11 sm:ml-5 lg:ml-10 xl:ml-16 font-extrabold">Home</h1>
+    <body className="bg-black flex flex-col">
+      <div className="mb-3.5 relative">
+      <div className=" flex justify-between items-center h-14 bg-[linear-gradient(140deg,_#B7B1F2,_#F8BDEB)] rounded-[4px]">
+        <h1 className=" ml-11 sm:ml-5 lg:ml-10 xl:ml-16 font-extrabold">HOME</h1>
         <nav className=" flex justify-end  w-4/6  ">
           <ul className="flex w-[100%] xl:ml-16 justify-evenly">
-            <li className=" relative after:absolute after:content-[''] after:w-0 after:bottom-0 after:left-0 after:bg-black after:h-0.5 rounded-3xl hover:after:w-full after:transition-all peer-hover:right-0 hover:">Home</li>
+            <li className=" relative after:absolute after:content-[''] after:w-0 after:bottom-0 after:left-0 after:bg-black after:h-0.5 rounded-3xl hover:after:w-full after:transition-all peer-hover:right-0 hover:text-gray-">Home</li>
             <li className="relative after:content-['']  after:absolute after:w-0 after:bg-black after:bottom-0 after:h-0.5 after:left-0 hover:after:w-full after:transition-all in-hover:right-0">Achivements</li>
             <li className="relative after:content-['']  after:absolute after:w-0 after:bg-black after:bottom-0 after:h-0.5 after:left-0 hover:after:w-full after:transition-all in-hover:right-0">Contact</li>
             <li className="relative after:content-['']  after:absolute after:w-0 after:bg-black after:bottom-0 after:h-0.5 after:left-0 hover:after:w-full after:transition-all in-hover:right-0"> About</li>
@@ -15,7 +15,18 @@ function Home() {
         </nav>
       </div>
     </div>
+
+    <h1 className=" text-center font-mono text-4xl font-extrabold text-white ml-6"><span>HELLO!</span> <br></br> IAM  <span className=" relative text-green-500 after:content-[''] after:absolute after:w-0 after:bottom-0 after:bg-black after:h-full after:right-0 hover:after:w-full after:transition-all after:duration-200">DAMODARA PRAKASH P</span> </h1>
+
+    <div className="flex justify-center">
+    <div  className=" mt-5 rounded-md relative flex justify-center items-center border w-72 h-72 border-white text-white" ><img src={profile} alt="Profile" /></div>
+    </div>
+    
+    </body>
+    
   );
 }
 
 export default Home;
+
+
