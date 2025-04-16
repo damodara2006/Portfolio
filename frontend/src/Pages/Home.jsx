374ecfa265a9d402be8202cfa5814882f8427b4a
damodara2006 <damodara2006@gmail.com>
2025-04-16 20:52:39 +0530

    Changed Resume

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -143,7 +143,7 @@ function Home() {
           <p className="text-white text-sm absolute right-0 mr-3">{width}</p>
           <span>HELLO!</span> <br />I AM{" "}
           <span className="relative text-green-500 after:content-[''] after:absolute after:w-0 after:bottom-0 after:bg-black after:h-full after:right-0  after:transition-all after:duration-200">
-            <>{name}</>
+            <>DAMODARA PRAKASH P</>
           </span>
         </h1>
         <div className="flex justify-center items-center w-full">
@@ -233,8 +233,8 @@ function Home() {
 
         <div className="mt-12" data-aos="fade-up" data-aos-duration="800">
           <a
-            href="/Damodara prakash Resume.pdf"
-            download="Damodara_prakash_Resume.pdf"
+            href="/DamodaraPrakash P_Resume.pdf"
+            download="DamodaraPrakash P_Resume.pdf"
             className="text-white bg-green-600 px-4 py-2 rounded-md active:bg-green-700 hover:bg-green-700  transition-all"
           >
             Download Resume
